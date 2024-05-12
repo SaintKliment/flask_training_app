2dffd39fb510d95178e2390e32f2ab132c671dd1
saintkliment <evgeniy122uryad@gmail.com>
2024-05-12 15:24:21 +0300

    add testInputs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 from config import host, user, password, db_name
 from flask_bootstrap import Bootstrap
@@ -18,11 +18,27 @@ def get_db_connection():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # conn = get_db_connection()
-    # with conn.cursor() as cursor:
-    #     cursor.execute('')
+    # conn.autocommit = True
+    # if request.method == 'POST':
+    #     form = request.form
+    #     name = form['name']
+    #     age = form['age']
+    #     print(age, name)
+    #     with conn.cursor() as cursor:
+    #         cursor.execute('INSERT INTO employee(name, age) VALUES(%s, %s)', (name, age))
 
     return render_template('index.html')
 
+# @app.route('/employees')
+# def employees():
+#     conn = get_db_connection()
+#     conn.autocommit = True
+#     with conn.cursor() as cursor:
+#             cursor.execute("SELECT * FROM employee")
+#             employees = cursor.fetchall()
+            
+#     print(employees)
+#     return render_template('employees.html', employees=employees)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
